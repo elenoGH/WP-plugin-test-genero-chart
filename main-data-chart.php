aea9ebef8737f2575c0b501e2076bba9d0c4bab5
elenoGH <goeleno@gmail.com>
2017-07-05 14:43:03 -0500

    archivo principal dode se genera los datos del plugin

---
+++ main-data-chart.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * Plugin Name: Graphic Manager
+ * Plugin URI:https://developer.wordpress.org/plugins/the-basics/
+ * Description: Manegador de graficas (Big Data Ine Genero)
+ * Version:1.0
+ * Author:Martin Eleno Perez
+ * Author URI:https://developer.wordpress.org/
+ * License:GPL2
+ * License URI:https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:wporg
+ * Domain Path:/prueba_plugin
+ * {Plugin Name} is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    any later version.
+
+    {Plugin Name} is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with {Plugin Name}. If not, see {URI to Plugin License}.
+
+ */
