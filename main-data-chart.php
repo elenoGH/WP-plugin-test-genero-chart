a7cff04e2781bfd110c7b96cd3549abcfbee3391
elenoGH <goeleno@gmail.com>
2017-07-10 19:30:09 -0500

    propuesta para subir archivo csv

--- main-data-chart.php
+++ main-data-chart.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Plugin Name: Graphic Manager Charts
  * Plugin URI:https://developer.wordpress.org/plugins/the-basics/
  * Description: Manegador de graficas (Big Data Genero)
@@ -12,20 +11,20 @@
  * Text Domain:wporg
  * Domain Path:/prueba_plugin
  * {Plugin Name} is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    any later version.
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  any later version.
 
-    {Plugin Name} is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  {Plugin Name} is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with {Plugin Name}. If not, see {URI to Plugin License}.
+  You should have received a copy of the GNU General Public License
+  along with {Plugin Name}. If not, see {URI to Plugin License}.
 
  */
-if ( is_admin() ) {
+if (is_admin()) {
     // we are in admin mode
     /**
      * Esta etiqueta condicional comprueba si el panel de control o el panel de 
@@ -36,6 +35,37 @@ if ( is_admin() ) {
      * Panel de control o el panel de administración. En su lugar, use current_user_can ().
      */
 //    require_once( dirname( __FILE__ ) . '/public/shortcodes-to-chart.php' );
+    global $wpdb;
+    $nombreTabla = $wpdb->prefix . "mcp_legislativo";
+    if ($wpdb->get_var("SHOW TABLES LIKE '$nombreTabla'") == $nombreTabla) {
+        /**
+         * Si la tabla existe...
+         * No haremos nada, pero se puede comprobar lo que hay actualizarlo por 
+         * si había una versión vieja, guardar una copia de lo que hay etc etc..
+         */
+    } else {
+        $sql = "CREATE TABLE ".$nombreTabla." ( "
+            . " id int(11) NOT NULL AUTO_INCREMENT "
+            . ", apellido varchar(33) DEFAULT NULL "
+            . ", nombre varchar(29) DEFAULT NULL "
+            . ", sexo varchar(6) DEFAULT NULL "
+            . ", partido_politico varchar(50) NOT NULL "
+            . ", principio_representacion varchar(27) DEFAULT NULL "
+            . ", distrito_electoral varchar(10) DEFAULT NULL "
+            . ", circunscripcion varchar(10) DEFAULT NULL "
+            . ", propietario_suplente varchar(11) DEFAULT NULL "
+            . ", periodo varchar(100) DEFAULT NULL "
+            . ", anio_ini int(11) DEFAULT NULL "
+            . ", anio_fin int(11) DEFAULT NULL "
+            . ", poder varchar(100) DEFAULT NULL "
+            . ", nivel_gobierno varchar(100) DEFAULT NULL "
+            . ", cargo varchar(100) DEFAULT NULL "
+            . ", descripcion_pp varchar(300) DEFAULT NULL "
+            . ", entidad_federativa varchar(100) DEFAULT NULL "
+            . ", PRIMARY KEY (id) "
+            . ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8; ";
+        $wpdb->query($sql);
+    }
 }
 
 
@@ -46,8 +76,6 @@ if ( is_admin() ) {
 //        require_once( dirname( __FILE__ ) . '/public/shortcodes-to-chart.php' );
 //    }
 //});
-
-
 ///**
 // * Activacion de un hook
 // * En la activación, los complementos pueden ejecutar una rutina para agregar 
@@ -66,4 +94,67 @@ if ( is_admin() ) {
 //register_deactivation_hook( __FILE__, 'plugin_f_a_tets_eleno_deactivation' );
 
 
-require_once( dirname( __FILE__ ) . '/public/shortcodes-to-chart.php' );
+require_once( dirname(__FILE__) . '/public/shortcodes-to-chart.php' );
+
+class import_csv {
+
+    function main_body() {
+        $string = 'Agregar solo archivo CSV';
+        echo $string;
+        ?>
+        <div class="wrap">
+            <h2>Import CSV Files</h2><br/>
+            <?php if ($this->error !== '') : ?>
+                <div class="error"><?php echo $this->error; ?></div>
+            <?php endif; ?>
+            <form class="add:the-list: validate" method="post" enctype="multipart/form-data">
+                <input name="_csv_import_files_next" type="hidden" value="next" />
+                <label for="csv_import">Select a CSV file:</label><br/>
+                <input name="csv_import" id="csv_import" type="file" value=""/>
+                <!--div id="formatdiv" class="postbox" style="display: block;max-width:350px;">
+                    <h3 class="hndle" style="cursor:auto;padding:10px;">
+                        <span>Select Import Type</span>
+                    </h3>
+                    <div class="inside">
+                        <div id="post-formats-select">
+                            <input id="post-format-0" class="post-format" type="radio" checked="checked" value="post" name="post_format">
+                            <label class="post-format-icon post-format-standard" for="post-format-0">
+                                Posts
+                            </label>
+                            <br>
+                            <input id="post-format-page" class="post-format" type="radio" value="page" name="post_format">
+                            <label for="post-format-page">&nbsp;&nbsp;Pages</label>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+                <div id="formatdiv" class="postbox" style="display: block;max-width:350px;">
+                    <h3 class="hndle" style="cursor:auto;padding:10px;">
+                        <span>
+                            Select Field Delimiter
+                        </span>
+                    </h3>
+                    <div class="inside">
+                        <div id="post-formats-select">
+                            <select name="field-delimiter">
+                                <option value=",">comma  ( , )</option>
+                                <option value=";">semicolon  ( ; )</option>
+                                <option value="	">tab  ( &nbsp;&nbsp;&nbsp;&nbsp; )</option>
+                            </select>
+                        </div>
+                    </div>
+                </div-->
+                <input type="submit" class="button" name="submit" value="Agregar" />
+            </form>
+        </div>
+        <?php
+    }
+
+}
+
+function get_action_class_import_csv() {
+    $plugin = new import_csv;
+    add_management_page("Datos CSV", "Importar Archivo CSV", "manage_options", "import_csv_files", array($plugin, 'main_body'));
+}
+
+add_action('admin_menu', 'get_action_class_import_csv');
